1d7fe92f8340904e24483f6266e95cb2677a76b8
first-name second-name <silvia.wachira@student.moringa.com>
2022-08-24 08:37:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("The DOM has loaded");
+//     const text = document.getElementById("text").value;
+//     text.innerHTML = "This is really cool!"
+// });
+// console.log("Before the DOM loads");
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    document.getElementById("text").innerHTML = "This is really cool!";
+    
+});
+console.log("Before the DOM loads");
